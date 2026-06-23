d58237796d7a782fd19f023e9894fc6ee18d61e9
aimanfiqinaja <aimanfiqinaja07@gmail.com>
2026-06-23 17:51:07 +0700

    Update supabase-config.js

--- public/supabase-config.js
+++ public/supabase-config.js
@@ -2,8 +2,9 @@
 // SUPABASE CONFIG – Ganti dengan kredensial project kamu!
 // Lihat PANDUAN.md untuk cara mendapatkan nilai ini.
 // ============================================================
-const SUPABASE_URL = "https://apijltjjtwabcyowlywp.supabase.co/rest/v1/";
-const SUPABASE_ANON = "sb_publishable_jDSn68zYVquB3NWgxUUt3Q_gZSMmMXX";
+const SUPABASE_URL = "https://apijltjjtwabcyowlywp.supabase.co";
+const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwaWpsdGpqdHdhYmN5b3dseXdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNTUxMzYsImV4cCI6MjA5NzczMTEzNn0.9jREAWkHqmfDJFowMgjcUqgvKPc0UX65ln-PgMi9_ZU
+";
 
 // Kredensial admin – ganti sesuai keinginan
 const ADMIN_CREDENTIALS = [
