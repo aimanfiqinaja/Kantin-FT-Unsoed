bd4c93b59827b7e5a27079fd5dbc373ff8d57985
aimanfiqinaja <aimanfiqinaja07@gmail.com>
2026-06-23 17:41:56 +0700

    Add files via upload

---
+++ public/supabase-config.js
@@ -0,0 +1,12 @@
+// ============================================================
+// SUPABASE CONFIG – Ganti dengan kredensial project kamu!
+// Lihat PANDUAN.md untuk cara mendapatkan nilai ini.
+// ============================================================
+const SUPABASE_URL = "https://uykijjkmaujymkvawzci.supabase.co/rest/v1/";
+const SUPABASE_ANON = "sb_publishable_XmPP-rGE88szpLQ3sE3LpA_3IFHl6St";
+
+// Kredensial admin – ganti sesuai keinginan
+const ADMIN_CREDENTIALS = [
+  { username: "admin", password: "kantin123" },
+  { username: "ftadmin", password: "unsoed2024" },
+];
